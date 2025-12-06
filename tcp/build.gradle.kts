201d924ceb3b198cd8de724458bca80bbbce411f
CC007 <coolcat_the_best2@hotmail.com>
2025-12-07 00:47:38 +0100

    Make it easier to call the client and server from the commandline

--- tcp/build.gradle.kts
+++ tcp/build.gradle.kts
@@ -6,3 +6,13 @@ dependencies {
     implementation(libs.bundles.kotlinxEcosystem)
     testImplementation(kotlin("test"))
 }
+
+tasks.register<JavaExec>("server") {
+    mainClass = "com.github.cc007.poc.network.TcpServerKt"
+    classpath = sourceSets["main"].runtimeClasspath
+}
+
+tasks.register<JavaExec>("client") {
+    mainClass = "com.github.cc007.poc.network.TcpClientKt"
+    classpath = sourceSets["main"].runtimeClasspath
+}
